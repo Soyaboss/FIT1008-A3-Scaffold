9122564690173547378fccaa365118082ba27098
Soyaboss <lwon0055@student.monash.edu>
2024-05-25 22:59:14 +0800

    Update mode1.py

--- mode1.py
+++ mode1.py
@@ -1,5 +1,6 @@
 from landsites import Land
-
+from data_structures.bst import BinarySearchTree
+from data_structures.bst import BSTInOrderIterator
 
 class Mode1Navigator:
     """
@@ -11,22 +12,49 @@ class Mode1Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.adventurers = adventurers
+        self.sites = sites
+        
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        bst  = BinarySearchTree()
+        num_adventurer = self.adventurers
+        list = []
+
+        for land in self.sites:
+            bst[land.get_guardians() / land.get_gold()] = land
+
+        in_order = [node.item for node in BSTInOrderIterator(bst.root)]
+
+        for land in in_order:
+            if num_adventurer >= land.get_guardians():
+                selected = land.get_guardians()
+                num_adventurer -= selected
+            
+            else:
+                selected = num_adventurer
+                num_adventurer = 0
+            list.append((land, selected))
+            
+            if selected == 0:
+                break
+        return list
+
+
+
+        
 
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
-
+        
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        land.set_gold(new_reward)
+        land.set_guardians(new_guardians)
